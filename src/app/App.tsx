a896f3020ffd4a9fd3b1a8e8e6e510ba06bc11b8
DanyXC01 <danilbelov0303@mail.ru>
2025-03-23 15:27:11 -0700

    Add custom theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from "@/shared/ui/button";
+import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main>
-      <header>
-        <img src={viteLogo} alt="Vite logo" />
-        <img src={reactLogo} alt="React logo" />
-        <h1>Vite + React</h1>
-      </header>
-
-      <section>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </section>
-
-      <footer>
-        <p>Проект с обнуленными стилями</p>
-      </footer>
+      <Button>Click me</Button>
     </main>
   );
 }
